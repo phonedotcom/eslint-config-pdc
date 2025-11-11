712ebd13ac13bc9af5e4fecc5077fd3c218180ff
Jacek Bajor <jbajor@gmail.com>
2025-11-11 10:41:32 +0100

    Update eslint.config.cjs

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,19 +1,24 @@
-const love = require('eslint-config-love').default
 const simpleImportSort = require('eslint-plugin-simple-import-sort')
+const tsEslint = require('@typescript-eslint/eslint-plugin')
+const tsParser = require('@typescript-eslint/parser')
+const love = require('eslint-config-love').default
 
 module.exports = [
   {
-    ...love,
-    files: ['**/*.js', '**/*.ts', '**/*.tsx'],
+    files: ['**/*.ts', '**/*.tsx', '**/*.js'],
     languageOptions: {
+      parser: tsParser,
       parserOptions: {
-        project: ['tsconfig.json']
+        project: ['./tsconfig.json'],
+        sourceType: 'module'
       }
     },
     plugins: {
+      '@typescript-eslint': tsEslint,
       'simple-import-sort': simpleImportSort
     },
     rules: {
+      ...love.rules,
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error'
     }
