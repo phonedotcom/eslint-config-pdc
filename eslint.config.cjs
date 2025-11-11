40337b50a2fae19f6f7f809676b3fe5c5db9f759
Jacek Bajor <jbajor@gmail.com>
2025-11-11 11:03:31 +0100

    Test

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,26 +1,51 @@
-const simpleImportSort = require('eslint-plugin-simple-import-sort')
-const tsEslint = require('@typescript-eslint/eslint-plugin')
-const tsParser = require('@typescript-eslint/parser')
-const love = require('eslint-config-love').default
+// const simpleImportSort = require('eslint-plugin-simple-import-sort')
+// const tsEslint = require('@typescript-eslint/eslint-plugin')
+// const tsParser = require('@typescript-eslint/parser')
+// const love = require('eslint-config-love').default
 
-module.exports = [
+// module.exports = [
+//   {
+//     files: ['**/*.ts', '**/*.tsx', '**/*.js'],
+//     languageOptions: {
+//       parser: tsParser,
+//       parserOptions: {
+//         project: ['./tsconfig.json'],
+//         sourceType: 'module'
+//       }
+//     },
+//     plugins: {
+//       '@typescript-eslint': tsEslint,
+//       'simple-import-sort': simpleImportSort
+//     },
+//     rules: {
+//       ...love.rules,
+//       'simple-import-sort/imports': 'error',
+//       'simple-import-sort/exports': 'error'
+//     }
+//   }
+// ]
+
+import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
   {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js'],
     languageOptions: {
-      parser: tsParser,
       parserOptions: {
         project: ['./tsconfig.json'],
         sourceType: 'module'
       }
     },
-    plugins: {
-      '@typescript-eslint': tsEslint,
-      'simple-import-sort': simpleImportSort
-    },
+    plugins: { "simple-import-sort": simpleImportSort },
     rules: {
-      ...love.rules,
-      'simple-import-sort/imports': 'error',
-      'simple-import-sort/exports': 'error'
-    }
-  }
-]
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+  eslintConfigPrettier,
+);
---
+++ eslint.config.ts
@@ -0,0 +1,24 @@
+import eslint from "@eslint/js"
+import eslintConfigPrettier from "eslint-config-prettier"
+import simpleImportSort from "eslint-plugin-simple-import-sort"
+import tseslint from "typescript-eslint"
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: { "simple-import-sort": simpleImportSort },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+  eslintConfigPrettier,
+)
