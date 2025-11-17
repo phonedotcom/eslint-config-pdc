843c83b6520da6ddcc0b2041928ce2fb30c65caf
Jacek Bajor <jbajor@gmail.com>
2025-11-17 09:34:06 +0100

    Switch to ts config

---
+++ eslint.config.ts
@@ -0,0 +1,19 @@
+import eslint from '@eslint/js'
+import { defineConfig } from 'eslint/config'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import tseslint from 'typescript-eslint'
+
+export default defineConfig([
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js'],
+    plugins: {
+      'simple-import-sort': simpleImportSort
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error'
+    }
+  }
+])
