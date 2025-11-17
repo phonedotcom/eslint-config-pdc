fa6b2122752064d3239fb410ddf06ef435628abc
Jacek Bajor <jbajor@gmail.com>
2025-11-17 10:57:00 +0100

    Updated rules (2)

--- eslint.config.ts
+++ eslint.config.ts
@@ -14,8 +14,7 @@ export default defineConfig([
     rules: {
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error',
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-unused-vars': 'warn'
+      '@typescript-eslint/no-explicit-any': 'warn'
     }
   }
 ])
